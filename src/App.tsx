8344dd8c919bb2b73ed6333e9f92efee45484d64
Nagi Seishiro <125158784+kaizen011@users.noreply.github.com>
2025-10-24 15:45:14 +0200

    fun

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,72 @@
-  import { useState } from 'react'
-  import nagi_Icon from '/nagi_icon.jpg'
-  import './App.css'
+import { useEffect, useState } from 'react'
+import nagi_Icon from '/nagi_icon.jpg'
+import './App.css'
 
-  function App() {
-    const [count, setCount] = useState(0)
+function App() {
+  const [count, setCount] = useState(0)
+  const footballers: string[] = ['Barou', 'Shidou', 'Sae', 'Isagi', 'Rin', 'Bachira']
+  const shoe: number = 24
+  const uuid = self.crypto.randomUUID();
 
-    return (
-      <>
-       
-         <img src={nagi_Icon} className="logo w-[300px] h-auto" alt="Nagi" /> 
-
-        <h1>Nagi Seishirō</h1>
-        <div className="card">
-          <button onClick={() => setCount((count) => count + 1)}>
-          Goals scored: {count}
-          </button>
-        </div>
-      </>
-    )
+  function alabama(hotpocket: number){
+    if(hotpocket >= 5){
+      console.log('i like hotpockets')
+    } else {
+      console.log(`not enough hotpockets, here, have an id: ${uuid}`)
+    }
   }
 
-  export default App
+  useEffect(() => {
+    alabama(4)
+  }, [])
+
+  const protArr: number[] = []
+  for (let i = -1; i < 20; i++) {
+   protArr.push(i)
+  }
+
+  const nickg: any= <div>hi</div>
+
+  const incrementedNumbers = protArr.map(number => (
+  <li key={number}>{number}</li>
+))
+
+  return (
+    <>
+    {nickg}
+    <ul>{incrementedNumbers}</ul>
+      <img src={nagi_Icon} className="logo w-[300px] h-auto border-2 border-blue-500" alt="Nagi" />
+
+      <h1>Nagi Seishirō</h1>
+      <div className="card">
+        <button onClick={() => setCount((c) => c + 1)}>
+          Goals scored: {count}
+        </button>
+
+        <div>Best footballer around:</div>
+        <List footballerList={footballers} footballerShoe={shoe} />
+      </div>
+    </>
+  )
+}
+
+interface ListItemInterface { footballer: string}
+function ListItem( {footballer}: ListItemInterface) {
+  return <li>{footballer}</li>
+}
+
+interface ListProps {
+  footballerList: string[]
+  footballerShoe?: number
+}
+function List({ footballerList }: ListProps) {
+  return (
+    <ul>
+      {footballerList.map((footballer) => (
+        <ListItem key={footballer} footballer={footballer} />
+      ))}
+    </ul>
+  )
+}
+
+export default App
